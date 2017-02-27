82e10b5f630ebf09749aa566b19f6fca3ad3e3aa
Intrepid <Intrepid@Sigma>
2017-02-26 23:21:44 -0800

    3rd commit

--- README.md
+++ README.md
@@ -10,8 +10,7 @@ PrismAboutUs.java
 - Includes main() method for running application
 
 
-
-* how to run your script
+* How to run your script
 - download Java SeleniumWeb driver http://www.seleniumhq.org/download/
 - download and install Selenium ChromeDriver http://www.seleniumhq.org/download/
 - add Selenium ChromeDriver to your system paths
@@ -19,13 +18,16 @@ PrismAboutUs.java
 - import SeleniumWeb libraries to the Java Build Path
 - run PrismAboutUs.java in IDE console
 
-* why you choose the language you did
+
+* Why you choose the language you did
 It looks like a 100% font end task and assuming JS will be the best fit for this type of tasks: manipulating with weblements.
 However there are libraries that allow to accomplish those types of tasks easy by using other programming languages. Final selection of Java was determined by knowledge compare to other languages.
+
+
 * what the primary design decisions were in implementing your solution and why you made the choices you did
-a) to GET webpage
-b) to parse webpage
-c) to store resulting data in the hashmap data structure as the most appropriate for that type of data.
+ -a) to GET webpage
+ -b) to parse webpage
+ -c) to store resulting data in the hashmap data structure as the most appropriate for that type of data.
  
-* if you ran out of time, what you were hoping to do next
-a) Probably it makes sense to make hashmap generic.
+* If you ran out of time, what you were hoping to do next
+ -a) Probably it makes sense to make hashmap generic.
--- src/selenium/PrismAboutUs.java
+++ src/selenium/PrismAboutUs.java
@@ -9,47 +9,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PrismAboutUs {
 
-    static Map<String, String> hashmap = new HashMap<String, String>();
-    
-    //java.util.List<java.util.Map.Entry<String,Integer>> pairList= new java.util.ArrayList<>();
+	static Map<String, String> hashmap = new HashMap<String, String>();
 
-    public static Map searchtext() {
-    	//System.setProperty("webdriver.chrome.driver", "C:\\Distirb\\Selenium\\chromedriver_win32\\chromedriver.exe");
+	public static Map searchtext() {
+		//System.setProperty("webdriver.chrome.driver", "C:\\Distirb\\Selenium\\chromedriver_win32\\chromedriver.exe");
 
-    	WebDriver driver = new ChromeDriver();
-    	driver.get("http://www.n2net.net/team-company");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.n2net.net/team-company");
 
 
-    	List<WebElement> element_names = driver.findElements(By.xpath("//div[@class='team-member-name']")); 
+		List<WebElement> element_names = driver.findElements(By.xpath("//div[@class='team-member-name']")); 
 
-    	List<WebElement> element_titles = driver.findElements(By.xpath("//div[@class='team-member-title']")); 
+		List<WebElement> element_titles = driver.findElements(By.xpath("//div[@class='team-member-title']")); 
 
-    	for (int i = 0; i < element_names.size(); i++) {
-    		hashmap.put(element_names.get(i).getText(), element_titles.get(i).getText());
-    	}
+		for (int i = 0; i < element_names.size(); i++) {
+			hashmap.put(element_names.get(i).getText(), element_titles.get(i).getText());
+		}
 
-    	/**
-    	 * For debuging purposes - printing WebElements names and titles
-    	 * 
+		/**
+		 * For debuging purposes - printing WebElements names and titles
+		 * 
     	 for(WebElement ele_n :element_names) for (WebElement ele_t :element_titles) {
     	 System.out.println(ele_n.getText());
     	 for(WebElement ele_t :element_titles) {
     	 System.out.println("Title===>"+ele_t.getText());}   	   
-    	 */
+		 */
 
-    	driver.quit();
-    	return hashmap;
-    	
-    }
+		driver.quit();
+		return hashmap;
 
-    public static void main(String[] args) {
+	}
 
-	    searchtext();
-	    
-	    for (Map.Entry entry : hashmap.entrySet()) {
-    	    System.out.println(entry.getKey() + ", " + entry.getValue());
-    	}
-		
+	public static void main(String[] args) {
 
-}
+		searchtext();
+
+		for (Map.Entry entry : hashmap.entrySet()) {
+			System.out.println(entry.getKey() + ", " + entry.getValue());
+		}
+
+
+	}
 }
